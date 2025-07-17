8c7a8735576b4e19f1eed86e451876071086fc5b
João Vitor Azevedo <marciano_store@outlook.com>
2025-07-17 01:30:20 -0300

    fix

--- trabalho_t3/gcc.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Rodando com entrada: %1 e saída: %2 >> log.txt
-java -jar "C:\Users\joao\Desktop\Projetos\construcao_de_compiladores\T1---Lucredio\trabalho_t3\target\trabalho_t3.jar" %1 %2
-
-
-
-
-
